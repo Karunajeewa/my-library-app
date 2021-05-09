4d96c74f84d5ab45fcf72443466bcbcd1ccdaf8b
Karunajeewa <karunajeewapws@gmail.com>
2021-05-09 21:50:14 +0530

    welcome section ui completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.scss';
 import Welcome from "./components/Welcome";
 
+
 const App:React.FC = ()=>{
   return(
-      <Welcome/>
+        <Welcome/>
   )
 };
 
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import {Col, Image, Row} from "react-bootstrap";
-import backgroundImg from '../assets/styles/images/backgroundImg.png'
+import {Col, Image, Row,} from "react-bootstrap";
+import backgroundImg from '../assets/images/backgroundImg.png'
 
 const Welcome:React.FC = () => {
     return(
-        <Row className='welcome'>
+        <Row className='welcome mw-100' >
             <Col xs={12} className='text-center py-2'>
                 <h1>My Library</h1>
             </Col>
-            <Col xs={12} className='text-center container-fluid'>
-                <Image src={backgroundImg} fluid/>
+            <Col  className=' lib-img bg-danger '>
+                <Image className='vw-100' src ={backgroundImg}/>
             </Col>
-            <Col xs={12} className='text-right py-2'>
-                    Photo by <a href=""/>
+            <Col xs={12} className='photo-credit'>
+                Photo by <a href='https://unsplash.com/photos/ajE5goOGzZc'>Anna Hunko</a>
+                on <a href='https://unsplash.com/photos/ajE5goOGzZc'>Unsplash</a>
             </Col>
         </Row>
     )
